2ed76f7db924c6f92960182adcb6629a61ed68f2
joerundh <joerundh.p01@gmail.com>
2025-05-14 13:42:44 +0200

    changed footer to more realistic one

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer>
             <p>
-                Jada
+                2025 No right reserved - Powered by the <a href="https://gutendex.com">Gutendex API</a>
             </p>
         </footer>
     )
