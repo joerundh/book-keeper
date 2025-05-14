62f4086e3acbcecdfd58a4bbd6c2ec136dd14453
joerundh <joerundh.p01@gmail.com>
2025-05-14 13:05:55 +0200

    started search handling, with api calling function

---
+++ src/pages/Search.jsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+
+const mapToParams = map => {
+    const params = new URLSearchParams();
+    map.forEach((key, value) => params.append(key, value));
+    return params;
+}
+
+async function searchApi(paramsMap) {
+    if (q) {
+        const res = await fetch(`https://gutendex.com/books?search=${mapToParams(paramsMap).toString()}}`);
+    }
+    return [];
+}
+
+export default function Search() {
+    const [ searchParams ] = useSearchParams();
+    const { data, isLoading, error } = useQuery({
+        queryKey: "bookSearch",
+        queryFn: searchApi,
+        enabled: searchParams.size
+    });
+    return (
+        ">"
+    )
+}
