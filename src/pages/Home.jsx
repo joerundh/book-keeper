cccf9c4a664c1965bccca7840435afd153a79bec
joerundh <joerundh.p01@gmail.com>
2025-05-27 20:59:14 +0200

    had to add some text to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 export default function HomePage() {
     return (
         <>
-            <h2>Home Page</h2>
+            <h2>Welcome to Book Keeper!</h2>
+            <p>This is an app for browsing the books available in the Gutendex API (<a href="https://gutendex.com" target="_blank">https://gutendex.com</a>), with the options of performing searches, browsing books in different categories and in different languages, setting up a list of favourite books, and also a reading list with status tracking.</p>
+            <p>To find books, use the search bar above for a simple search, or navigate using the drop-down menu to "Advanced Search" where options for categories and languages are included.</p>
+            <p>Also available are lists of books in given categories (see the drop-down menu to go straight to a category), and for books in other available languages.</p>
+            <p>Disclaimer: This page is best viewed in dark mode.</p>
         </>
     )
 }
