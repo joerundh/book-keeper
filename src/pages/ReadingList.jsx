4711ebcbcae5911a1fda5f210c75fc8c4df51792
joerundh <joerundh.p01@gmail.com>
2025-05-27 10:21:15 +0200

    reading list view and sort works, so i suppose reading list works fine as well

--- src/pages/ReadingList.jsx
+++ src/pages/ReadingList.jsx
@@ -1,12 +1,63 @@
 import { BookContext } from "../App"
-import { useContext, useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import ReadingListItem from "../components/ReadingListItem";
 import InlineList from "../components/InlineList";
 
 export default function ReadingList() {
-    const { readingList, isInReadingList, removeFromReadingList } = useContext(BookContext);
+    const { readingList } = useContext(BookContext);
 
-    const [ view, setView ] = useState({ 0: true, 1: true, 2: true, 3: true });
+    const [ view, setView ] = useState(JSON.parse(localStorage.getItem("Book Keeper Reading List View") || `{ "0": true, "1": true, "2": true, "3": true }`));
+    useEffect(() => {
+        if (Object.is(view, { 0: true, 1: true, 2: true, 3: true })) {
+            localStorage.removeItem("Book Keeper Reading List View");
+        } else {
+            localStorage.setItem("Book Keeper Reading List View", JSON.stringify(view));
+        }
+    }, [ view ]);
+
+    const [ sort, setSort ] = useState(JSON.parse(localStorage.getItem("Book Keeper Reading List Sort") || "0"));
+    useEffect(() => {
+        if (sort) {
+            localStorage.setItem("Book Keeper Reading List Sort", `${sort}`);
+        } else {
+            localStorage.removeItem("Book Keeper Reading List Sort");
+        }
+    }, [ sort ]);
+
+    const sortFn = [
+        {
+            label: "Title, A-Z",
+            fn: (entry1, entry2) => entry1.book.title.localeCompare(entry2.book.title)
+        },
+        {
+            label: "Title, Z-A",
+            fn: (entry1, entry2) => entry2.book.title.localeCompare(entry1.book.title)
+        },
+        {
+            label: "Author, A-Z",
+            fn: (entry1, entry2) => entry1.book.authors[0].name.localeCompare(entry2.book.authors[0].name)
+        },
+        {
+            label: "Author, Z-A",
+            fn: (entry1, entry2) => entry2.book.authors[0].name.localeCompare(entry1.book.authors[0].name)
+        },
+        {
+            label: "Status",
+            fn: (entry1, entry2) => entry1.status - entry2.status
+        },
+        {
+            label: "Status, reversed",
+            fn: (entry1, entry2) => entry2.status - entry1.status
+        },
+        {
+            label: "Time added",
+            fn: (entry1, entry2) => entry1.timeAdded - entry2.timeAdded
+        },
+        {
+            label: "Time added, reversed",
+            fn: (entry1, entry2) => entry2.timeAdded - entry1.timeAdded
+        }
+    ]
 
     const toggleCheckbox = (e, status) => {
         const newView = Object.assign({}, view);
@@ -44,6 +95,8 @@ export default function ReadingList() {
         gap: 20
     }
 
+    const list = readingList.filter(entry => view[entry.status]).sort((entry1, entry2) => sortFn[sort].fn(entry1, entry2))
+
     return (
         <>
             <div style={componentCSS}>
@@ -58,13 +111,19 @@ export default function ReadingList() {
                     </InlineList>
                 </div>
                 <div style={sortCSS}>
-                    <p>sort</p>
+                    <label>Sort by: <select defaultValue={sort} onChange={e => setSort(Number.parseInt(e.target.value))} style={{ padding: 3 }}>
+                        {
+                            sortFn.map((obj, index) => (
+                                <option key={index} value={index}>{obj.label}</option>
+                            ))
+                        }
+                    </select></label>
                 </div>
                 <ul style={listCSS}>
                     {
-                        readingList.length ? readingList.map((entry, index) => (
+                        list.length ? list.map((entry, index) => (
                             <ReadingListItem key={index} entry={entry} /> 
-                        )) : <p></p>
+                        )) : <p style={{ textAlign: "center" }}>(No items.)</p>
                     }
                 </ul>
             </div>
