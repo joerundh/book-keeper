f3217814e8b54b1caca1e80014e119b650aba461
joerundh <joerundh.p01@gmail.com>
2025-05-28 13:06:52 +0200

    updated loading icon component to include icon when deploying

--- src/components/LoadingIcon.jsx
+++ src/components/LoadingIcon.jsx
@@ -1,14 +1,15 @@
+import loadingIcon from "../assets/loader-icon.png";
+
 export default function LoadingIcon({ width }) {
     const loadingIconCSS = {
         width: width,
         height: width,
         borderRadius: width/2,
         backgroundColor: "grey",
-        maskImage: `url("${location.origin}/src/assets/loader-icon.png")`,
+        maskImage: `url("${loadingIcon}")`,
         maskRepeat: "no-repeat",
         maskSize: "100% 100%",
         animation: "rotate 1500ms linear infinite"
-        //animation: { name: "rotate", duration: "5s", timingFunction: "linear", iterationCount: "infinite" }
     };
 
     return (
